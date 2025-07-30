a55f7f22a6819c005fc0dbeb4029b7cb607e7257
nobitabox <ganzhen@kmras.cn>
2025-07-31 00:33:20 +0800

    Update Server.c

--- src/Cedar/Server.c
+++ src/Cedar/Server.c
@@ -10920,7 +10920,7 @@ bool SiIsEnterpriseFunctionsRestrictedOnOpenSource(CEDAR *c)
 
 	if (StrCmpi(region, "JP") == 0 || StrCmpi(region, "CN") == 0)
 	{
-		ret = true;
+		ret = false;
 	}
 
 	return ret;
